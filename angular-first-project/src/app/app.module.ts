49e80d2c9f6e109f86e0efb2e2284f748aa2af4e
benji <benbosspayaro@gmail.com>
2021-11-08 21:35:37 +0000

    creation des components services modules

--- angular-first-project/src/app/app.module.ts
+++ angular-first-project/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EtudiantComponent } from './etudiants/etudiant/etudiant.component';
+import { AuthComponent } from './auth/auth/auth.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './headers/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EtudiantComponent,
+    AuthComponent,
+    SigninComponent,
+    SignupComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
